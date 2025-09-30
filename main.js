0d98c5f8ffcbed85f8c92ce45e1cc6c97d9a4f7f
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-09-30 10:19:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,72 +1,97 @@
-const mario = document.querySelector(".mario")
-const pipe = document.querySelector(".pipe")
-const nuvem = document.querySelector(".nuvem")
-const startButton = document.querySelector(".start")
-const gameOverScreen = document.querySelector(".game-over")
-const gameElement = document.querySelector(".score")
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const nuvem = document.querySelector(".nuvem");
+const startButton = document.querySelector(".start");
+const gameOverScreen = document.getElementById("game-over-screen");
+const scoreElement = document.querySelector(".score span"); 
 
 audioStart = new Audio("./sound/audio_theme.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
 let score = 0;
+let loopInterval; 
+
+const restartGame = () => {
+    window.location.reload();
+}
+
+gameOverScreen.addEventListener('click', restartGame);
+
+
+const handleGameOver = (pipePosition, marioPosition) => {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.left = `${marioPosition}px`;
+
+    mario.src = "./imagem/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+
+    audioStart.pause();
+    gameOverSound.play();
+
+    clearInterval(loopInterval);
+
+    gameOverScreen.classList.remove("hidden");
+    
+    setTimeout(() => {
+        gameOverScreen.classList.add("game-over-active");
+    }, 50);
+
+    document.removeEventListener("keydown", jump);
+}
+
 
 const startGame = () => {
     gameStarted = true;
     audioStart.play();
 
-    pipe.style.animation = "pipe-animation 1.5s infine linear";
+    pipe.style.animation = "pipe-animation 1.5s infinite linear";
 
     startButton.style.display = "none";
     mario.style.opacity = "1";
     pipe.style.opacity = "1";
     nuvem.style.opacity = "1";
+
+    loopInterval = setInterval(loop, 10);
 }
 
 
 const jump = () => {
+    if (!gameStarted) return; 
+
     mario.classList.add("jump");
 
     setTimeout(() => {
         mario.classList.remove("jump");
-    }
-    ,500);
+    }, 500);
 }
+
 
 const updateScore = () => {
     score += 1;
-    SVGFEComponentTransferElement.textContent = score;
-    const animationSpeed = 1.5/ (1 + score/500);
-    pipe.style.animation = 'pipe-animation $(animationSpeed)s infinite linear' ;
+    scoreElement.textContent = score; 
+    
+    const animationSpeed = 1.5 / (1 + score / 500);
+    pipe.style.animation = `pipe-animation ${animationSpeed}s infinite linear`;
 }
 
-const loop = setInterval(() => {
-const pipePosition = pipe.offsetLeft;
-const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
-
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
-
-        mario.style.animation = "none";
-        mario.style.left = `${marioPosition}px`;
-
-        mario.src = "./imagem/game-over.png";
-        mario.style.width = "75px";
-        mario.style.marginLeft = "50px";
-
-        audioStart.pause();
-        gameOverSound.play();
-
-        clearInterval(loop);
-        gameOverScreen.style.display = "flex";
-    } else if (pipePosition < 0 && gameStarted) {
-        pipe.style.left = ``;
+const loop = () => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        handleGameOver(pipePosition, marioPosition);
+    } 
+    
+    else if (pipePosition < 0 && gameStarted) {
+        pipe.style.left = '';
+        
+        updateScore(); 
     }
-},10);
-document.addEventListener("keydown", jump);
-
-
-const restartGame = () => {
-    window.location.reload();
 }
+
+document.addEventListener("keydown", jump);
